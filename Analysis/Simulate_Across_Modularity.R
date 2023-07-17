6341b324043151bbc5f256b924fb1d511f2103f8
Your Name <you@example.com>
2023-07-17 15:56:11 -0400

    More polished draft of modularity scaling code

--- Analysis/Simulate_Across_Modularity.R
+++ Analysis/Simulate_Across_Modularity.R
@@ -9,145 +9,74 @@ library(simplifyNet)
 nsites <- 100
 
 
-A <- matrix(data=runif(nsites^2, 0,1),ncol=nsites)
-Asym <- A %*% t(A)
-class(Asym/max(Asym))
-
-nsites <- 60
-
-
-
 wi <-0.4 #Bernoulli probability of link within module
 ac <-0.01 #Bernoulli probability of link between module
-pref <- matrix(data=rep(ac, (nsites/20)^2), ncol=nsites/20, byrow = TRUE)
-diag(pref) <- wi
-a <- igraph::sample_sbm(nsites, pref.matrix=pref, block.sizes = rep(20, nsites/20)) #Use sample_sbm to make a random graph
+pref <- matrix(data=rep(ac, (nsites/25)^2), ncol=nsites/25, byrow = TRUE) #Create sbm preference matrix according to above probabilities
+diag(pref) <- wi 
+a <- igraph::sample_sbm(nsites, pref.matrix=pref, block.sizes = rep(25, nsites/25)) #Use sample_sbm to make a random graph
 com <- igraph::fastgreedy.community(a) #Find community identity
-V(a)$color <- com$membership
+V(a)$color <- com$membership #assign it to color
 
-
-coords <- layout_nicely(a)
+coords <- layout_nicely(a) #use layout-nicely to enforce 2D spatial orientation
 coords[,1] <- coords[,1]/max(abs(coords[,1])) #rescale to between -1 to 1 so dexp is comparable to my random graphs
 coords[,2] <- coords[,2]/max(abs(coords[,2]))#rescale to between -1 to 1 so dexp is comparable to my random graphs
-eucdist <- dist(coords, diag=T, upper=T)#r
+eucdist <- dist(coords, diag=T, upper=T) #Create distance matrix from coordinates. 
 dexpdist <- dexp(eucdist, rate=2.5) #This rate parameter is somewhat arbitrarily chosen. Used 10 in other simulations, but relized this gives a bunch of nearly 1 connectivities.
 dexpsim <- as.matrix(dexpdist/max(dexpdist)) #NOTE: Changed from previous formulation. Realized that a distance score wasn't great to combine with network weighting below
+#plot(a, layout=coords)
+
+empty <- matrix(data=1, nrow=nsites, ncol=nsites) #Create fully-connected adjacency matrix
+diag(empty) <- 0 #remove self-loops
+graph <- graph_from_adjacency_matrix(adjmatrix = empty, mode=c("undirected")) #turn it into a graph
+E(graph)$weight <- dexpsim[t(combn(nrow(dexpsim), 2))] #assign weights (connectivitied) based on the exponential distance kernel
+
+V(graph)$color <- com$membership #Assign membership using the ORIGINAL fastgreedy optimization for visualization
+
+#plot(graph, layout=coords) #This graph is super dense; going to reduce using bestpath sparsification (package by Kramer, 2022, method by Spielman et al., 2013)
+#g2 <- delete.edges(graph, which(E(graph)$weight < quantile(E(graph)$weight, .8))) #Trim down to only the closest 20% links-old, really coarse sparsification method
+
+#plot(a, layout=coords)
+
+s <- simplifyNet::bestpath(graph, associative = TRUE) #Conduct sparsification
+sg <- simplifyNet::net.as(s, net.to="igraph", directed=FALSE) #return sparsified graph as igraph object
+#plot(sg, layout=coords)
+igraph::ecount(sg)/igraph::ecount(graph)#fraction of edges in the sparsified network
+
+classifysg <- fastgreedy.community(sg, weights=E(sg)$weight) #Recalculate community membership. Since we've changed our structure by fully connecting and then sparsifying, we need to reassign communities to avoid artifically reducing modularity score
+modularity(sg, membership = classifysg$membership) #Quite modular network. Next step is to degrade its modularity by randomly reassigning links
 
 
-empty <- matrix(data=1, nrow=nsites, ncol=nsites)
-diag(empty) <- 0
-graph <- graph_from_adjacency_matrix(adjmatrix = empty, mode=c("undirected"))
-E(graph)$weight <- dexpsim[t(combn(nrow(dexpsim), 2))]
+sgdeg <- sg 
+modularity(sg, membership = classifysg$membership) #What's our starting modularity?
+output <- NULL
+ndegrades <- 1000 #How many links should we redistribute?
+failcount <- 0
+i <- 1
+brake <- 0
 
-V(graph)$color <- com$membership
-igraph::fastgreedy.community(graph, weights=E(graph)$weight)
-igraph::fastgreedy.community(g2, weights=E(g2)$weight)
+while(i<500){
+  brake <- brake +1 #Make sure we don't get stuck here
+  if(brake > ndegrades*1000){
+    print("too many failed reassignments-stuck in while loop")
+    break()
+  }
+  new_edge <- sample(1:length(V(sgdeg)), size=2, replace=FALSE) #Randomly choose two new vertices to connect
+  if(are.connected(sgdeg, v1=new_edge[1], v2=new_edge[2])==TRUE){ #Check if that new edge already exists in the network
+    next() #If it does already exist, move to the next iteration. NEED TO MAKE THIS TRY AGAIN RATHER THAN JUST GIVE UP
+    }
+  
+  old_edge <- sample(1:length(E(sgdeg)), size=1) #Choose an old edge to delete
+  old_weight <- E(sgdeg)$weight[old_edge] #extract its weight
+  sgdeg <- delete_edges(sgdeg, edges=old_edge) #Delete it from the graph
+  
+  sgdeg <- add_edges(sgdeg, edges=new_edge, weight=old_weight) #Add in the new edge, with its new weight equal to the deleted (reassigning existing edge set)
+  temp <- data.frame(t=i,mode=modularity(sgdeg, membership=classifysg$membership)) #return the number of edges reassigned, plus the new modularity score
+  output <- rbind(output, temp) 
+  i <- i+1
+}
 
-plot(graph, layout=coords)
-g2 <- delete.edges(graph, which(E(graph)$weight < quantile(E(graph)$weight, .8))) #Trim down to only the closest 20% links
-E(graph)[E(graph)$weight==min(E(graph)$weight)]
-
-plot(graph, layout=coords)
-plot(g2, layout=coords)
-plot(a, layout=coords)
-
-s <- simplifyNet::bestpath(graph, associative = TRUE)
-sg <- simplifyNet::net.as(s, net.to="igraph", directed=FALSE)
-plot(sg, layout=coords)
-igraph::ecount(sg)/igraph::ecount(graph)#fraction of edges in the sparsifier
-
-classifysg <- fastgreedy.community(sg, weights=E(sg)$weight)
-modularity(sg, membership = classifysg$membership)
-as.factor(classifysg$membership)
-as.factor(com$membership)
-output <- data.frame(v1=classifysg$membership, v2=com$membership)
-table(output)
-
-
-#Sparsify using Effective Resistances
-effR <- simplifyNet::EffR(graph)
-Eff <- EffRSparse(graph, 110, effR, seed=24601)
-Effg <- simplifyNet::net.as(Eff, net.to="igraph", directed=F)
-igraph::ecount(Effg)/igraph::ecount(graph)
-modules <- igraph::fastgreedy.community(Effg, weights=E(Effg)$weights) #Similiar to my problem before, the new sparsified graph shows different modules than the sbm graph that it was buil upon :/
-length(V(Effg))
-length(V(graph))
-
-E(Effg)$weight
-E(graph)$weight
-is_connected(Effg)
-plot(Effg, layout=coords)
-length(V(Effg))
+plot(x=output$t, y=output$mode)
 #####################################################################################
-
-
-
-coords <- layout_nicely(a)
-#coords <- igraph::layout_with_kk(a)
-coords
-coords[,1] <- coords[,1]/max(abs(coords[,1])) #rescale to between -1 to 1 so dexp is comparable to my random graphs
-coords[,2] <- coords[,2]/max(abs(coords[,2]))#rescale to between -1 to 1 so dexp is comparable to my random graphs
-eucdist <- dist(coords, diag=T, upper=T)#r
-dexpdist <- dexp(eucdist, rate=2.5) #This rate parameter is somewhat arbitrarily chosen. Used 10 in other simulations, but relized this gives a bunch of nearly 1 connectivities.
-dexpsim<- 1/(1+as.matrix(dexpdist)) #This has a full distance matrix. Do I only assign the values for the sites connected in my original graph, or do I let them all have pairwise connections?
-hist(x)
-hist(x[x>quantile(0.8, x)])
-
-
-
-empty <- matrix(data=1, nrow=nsites, ncol=nsites)
-diag(empty) <- 0
-dexpsim[upper.tri(dexpsim)==TRUE]
-
-graph <- graph_from_adjacency_matrix(adjmatrix = empty, mode=c("undirected"))
-
-
-
-
-triu(dexpsim)[triu(dexpsim)!=1][1:3]
-dexpsim[upper.tri(dexpsim)==TRUE][1:3]
-dexpsim[1:4,1:4]
-?triu
-result <- dexpsim[t(combn(nrow(dexpsim), 2))] #This code extracts the non-diagonal upper triangle values of the distance matrix rowwise (so they're in an order that can be assigned to the rowweights)
-
-tail(a)
-E(graph)$weights <- dexpsim[t(combn(nrow(dexpsim), 2))] #This code extracts the non-diagonal upper triangle values of the distance matrix rowwise (so they're in an order that can be assigned to the rowweights)
-
-fastgreedy.community(graph, weights=E(graph)$weights)
-cluster_edge_betweenness(graph, weights=E(graph)$weights)
-
-#Problem: We create a modular graph that contains 5 submodules using sample_sbm(). However, this only creates 0-1 edges (no weight)
-#To make weights approximating a real-world landscape, we layout_nicely this graph, turn the coordinates into a euclidean distance matrix, run that through a exponential decay
-#And now we have a new graph, fully connected, where edgeweights are equivalent to the connectivity between sites.
-#The only downside to this is that due to this process, we no longer really have the same number of modules, and I'm not sure how much variability in modularity we have to control
-#When we degrade networks from a high modularity, the step probably needs done at the last step. I guess I'll do this by randomly swapping network link weights?
-quantile(E(graph)$weights,0.8)
-g2 <- delete.edges(graph, which(E(graph)$weights < quantile(E(graph)$weights,0.95))) #This should be trimming down to only the 20% of closest links, but seems random. Looks like link weight is not being assigned correctly indexed.
-hist(E(graph)$weights)
-plot(a, layout=coords)
-plot(g2, layout=coords)
-igraph::modularity(graph, membership=fastgreedy.community(graph, weights=E(graph)$weights)$membership, weights=E(graph)$weights) #Get resulting modularity. 
-
-plot(graph, layout=coords)
-E(a)$weights[1]
-
-val <- E(a)[1]
-
-dists <- distance_table(a, directed = FALSE)
-dists <- igraph::distances(a)
-
-range(dexpsim)
-
-factor_sites <- as.factor(1:nsites) #This is important for the dispersal function down the road
-coords <- data.frame(x=runif(nsites), y=runif(nsites))
-eucdist <- dist(coords, diag=T, upper=T)
-dexpdist <- dexp(eucdist, rate=20)
-dexpsim <- 1/(1+as.matrix(dexpdist)) #Converting our distance matrix to a similarity score (easier for me to then turn into a pmf).
-range(dexpsim)
-
-#Found this particular metric from here: https://stats.stackexchange.com/questions/158279/how-i-can-convert-distance-euclidean-to-similarity-score
-
-
 
 
 
